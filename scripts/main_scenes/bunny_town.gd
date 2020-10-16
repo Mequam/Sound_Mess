2d14c31df39aba04f8ca55150360c9f0e10eb833
Mequam <blue9ja@gmail.com>
2020-10-15 21:03:00 -0400

    added the start of bunny town, a new windmill and daisies

---
+++ scripts/main_scenes/bunny_town.gd
@@ -0,0 +1,16 @@
+extends "res://scripts/abstracts/bunny_forest.gd"
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	.init()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
