bdf850ad4660f5079ec6625b6ba02997ffdac1c9
Mequam <blue9ja@gmail.com>
2021-05-04 09:00:21 -0400

    whiped up a node for circular rotation for the planed procedural animation of centipiedes

---
+++ scripts/CircleRail.gd
@@ -0,0 +1,88 @@
+extends Node2D
+#this node controls the rotational movement
+#that will be used by the centipide enemy
+
+#you can think of this node as the "pure"
+#haskell like implimentation of the idea
+#of a rotating node, it is inteaended to be
+#overidden with update functions and state 
+
+#TODO: this would be a good canidit for 
+#a C++ godot module for the sin cosin behavior
+
+
+#offset from node parent
+var center : Vector2 = Vector2(0,0) setget set_center,get_center
+func set_center(val : Vector2):
+	center = val
+	sync_state()
+func get_center()->Vector2:
+	return center
+	
+	
+#radius of our circle
+var r : float = 100 setget set_r,get_r
+func set_r(val : float)->void:
+	r = val
+	sync_state()
+func get_r() -> float:
+	return r
+	
+#the 3d rotation angle of our path
+#posotive values rotate the top of a circle away
+var angle_z : float = 0 setget set_angle_z,get_angle_z
+func set_angle_z(val : float)->void:
+	angle_z = val
+	sync_state()
+func get_angle_z()->float:
+	return angle_z
+
+#angle along the circle that we are located
+var angle : float = PI/2 setget set_angle,get_angle
+func set_angle(val : float)->void:
+	angle = val
+	sync_state()
+func get_angle()->float:
+	return angle
+
+#distance beetween the camera and the point of focus
+var cam_dist : float = 150 setget set_cam_dist,get_cam_dist
+func set_cam_dist(val : float)->void:
+	cam_dist = val
+	sync_state()
+func get_cam_dist()->float:
+	return cam_dist
+
+#how far from z=0 the focus point is, imagine it 
+#stabbing you in the face from the computer screen 
+var focus_offset : float = 150 setget set_focus_offset,get_focus_offset
+func set_focus_offset(val : float)->void:
+	focus_offset = val
+	sync_state()
+func get_focus_offset()->float:
+	return focus_offset
+
+func get_pos_perspective(angle : float,angle_z : float,r : float,center : Vector2) -> Vector2:
+	return Vector2(cos(angle),cos(angle_z)*sin(angle))*r+center
+#gets the scale of a sphear at the given rotations
+func get_scaled_perspective(angle : float,
+							angle_z : float,
+							cam_dist : float,
+							focus_offset : float,
+							r : float)->float:
+	return cam_dist/(focus_offset+sin(angle)*sin(angle_z)*r)
+#updates the position and scale values of our node
+func update_state(angle : float,angle_z : float,
+				cam_dist : float,focus_offset : float,
+				r : float,center : Vector2)->void:
+	position = get_pos_perspective(angle,angle_z,r,center)
+	scale = Vector2(1,1)*get_scaled_perspective(angle,angle_z,
+												cam_dist,focus_offset,r)
+#syntactic sugar to sync with our state
+func sync_state()->void:
+	update_state(angle,angle_z,cam_dist,focus_offset,r,center)
+
+#convienence ready function that uses the editor given  position as our
+#initial center
+func _ready():
+	center = position
